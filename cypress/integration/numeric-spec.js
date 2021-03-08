59c34b9fe0ef36e7704a2c07a62640f28610cf67
Robbie Nohra <email@robbienohra.com>
2021-03-07 23:02:46 -0500

    updated readme

--- cypress/integration/numeric-spec.js
+++ cypress/integration/numeric-spec.js
@@ -1,5 +1,7 @@
-it('click 1', () => {
+it('1 + 2', () => {
   cy.visit('http://localhost:3000/');
   cy.get('[data-cy=1]').click();
-  cy.get('[data-cy=display]').contains('1');
+  cy.get('[data-cy=+]').click();
+  cy.get('[data-cy=2]').click();
+  cy.get('[data-cy=display]').contains('3');
 });
