ecfd2590e7b00db4fde4d770fa619f5d2cb7e619
Robbie Nohra <email@robbienohra.com>
2021-03-07 23:06:55 -0500

    added preliminary cypress testing

--- cypress/integration/numeric-spec.js
+++ cypress/integration/numeric-spec.js
@@ -1,7 +1,8 @@
 it('1 + 2', () => {
   cy.visit('http://localhost:3000/');
-  cy.get('[data-cy=1]').click();
-  cy.get('[data-cy=+]').click();
-  cy.get('[data-cy=2]').click();
+  cy.get('[data-cy="1"]').click();
+  cy.get('[data-cy="+"]').click();
+  cy.get('[data-cy="2"]').click();
+  cy.get('[data-cy="="]').click();
   cy.get('[data-cy=display]').contains('3');
 });
