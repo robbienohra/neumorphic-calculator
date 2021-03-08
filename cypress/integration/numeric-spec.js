973c46677dde507735a21fff9827173f866cdb83
Robbie Nohra <email@robbienohra.com>
2021-03-08 01:38:25 -0500

    added multiplication test

--- cypress/integration/numeric-spec.js
+++ cypress/integration/numeric-spec.js
@@ -11,6 +11,14 @@ describe('arithmetic operations', () => {
     cy.get('[data-cy=display]').contains('3');
   });
 
+  it('2 x 3', () => {
+    cy.get('[data-cy="2"]').click();
+    cy.get('[data-cy="x"]').click();
+    cy.get('[data-cy="3"]').click();
+    cy.get('[data-cy="="]').click();
+    cy.get('[data-cy=display]').contains('6');
+  });
+
   it('1 -> -1', () => {
     cy.get('[data-cy="1"]').click();
     cy.get('[data-cy="+/-"]').click();
